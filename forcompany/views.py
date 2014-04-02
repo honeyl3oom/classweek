9e622225b9d1890de9838c8b3be2182888e70def
parkjuram <parkjuram@gmail.com>
2014-04-02 18:03:20 +0900

    ready to delpoy in real server

--- forcompany/views.py
+++ forcompany/views.py
@@ -5,4 +5,10 @@ from django.http import HttpResponse
 from forcompany.forms import CompanyInfoForm
 
 def fill_info(request):
+
+    if request.method == 'POST':
+        form = CompanyInfoForm(data=request.POST)
+        if form.is_valid():
+            print 'valid success'
+
     return render(request, 'fill_info.html', {'form': CompanyInfoForm()})
