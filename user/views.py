01bca52848d31fe171ad2d59fc08b9070842c0bc
parkjuram <parkjuram@gmail.com>
2014-04-03 19:54:23 +0900

    edit user views.py

--- user/views.py
+++ user/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 from forcompany.forms import CompanyInfoForm
 
 def sign_up(request):
-    pass
+    response_data = {}
+    response_data['result'] = 'success'
+    response_data['message'] = 'sign up success'
+    return HttpResponse( json.dumps(response_data), content_type="application/json")
