21fffad02855a394f9d7cf3935304531512f4c39
parkjuram <parkjuram@gmail.com>
2014-04-09 13:46:01 +0900

    user url change

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^login_or_registration$', 'user.views.login_or_registration', name='login_or_registration'),
-    url(r'^login$', 'user.views.login_view', name='login'),
     url(r'^login_test$', 'user.views.login_test', name='login_test'),
     url(r'^logout$', 'user.views.logout_view', name='logout'),
 )
