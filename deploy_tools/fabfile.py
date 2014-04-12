25f0561754ac43c9bb6b6d2475807320a180861d
parkjuram <parkjuram@gmail.com>
2014-04-12 19:02:30 +0900

    change fab file

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -59,6 +59,9 @@ def _update_static_files(source_folder):
 
 def _update_database(source_folder):
     run('cd %s && ../virtualenv/bin/python manage.py syncdb --noinput' % (source_folder,))
+    # run('cd %s && ../virtualenv/bin/python manage.py schemamigration classes --initial' % (source_folder,))
+    # run('cd %s && ../virtualenv/bin/python manage.py schemamigration classes --auto' % (source_folder,))
+    run('cd %s && ../virtualenv/bin/python manage.py migrate classes' % (source_folder,))
     # one-off fake database migration. remove me before next deploy
     # run('cd %s && ../virtualenv/bin/python manage.py migrate lists --fake 0001' % (
         # source_folder,
