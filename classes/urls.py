620b452eefcf1f2d623fc424f3ba42698f3a3dfd
parkjuram <parkjuram@gmail.com>
2014-04-18 18:24:14 +0900

    url underbar add

--- classes/urls.py
+++ classes/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^([A-Za-z]+)$', 'classes.views.getSubCategoryList_view', name='getSubCategoryList'),
-    url(r'^([A-Za-z]+)\/([A-Za-z]+)$', 'classes.views.getClassesList_view', name='getClassesList'),
-    url(r'^([A-Za-z]+)\/([A-Za-z]+)\/(\d+)$', 'classes.views.getClassesList_view', name='getClassesList'),
+    url(r'^([A-Za-z_]+)$', 'classes.views.getSubCategoryList_view', name='getSubCategoryList'),
+    url(r'^([A-Za-z_]+)\/([A-Za-z_]+)$', 'classes.views.getClassesList_view', name='getClassesList'),
+    url(r'^([A-Za-z_]+)\/([A-Za-z_]+)\/(\d+)$', 'classes.views.getClassesList_view', name='getClassesList'),
     url(r'^(\d+)\/(\d+)$', 'classes.views.getClassesDetail_view', name='inquire'),
     url(r'^(\d+)\/inquire$', 'classes.views.inquire_view', name='inquire'),
 )
