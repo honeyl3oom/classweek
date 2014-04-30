a55cf33827eb2532ee36e2df1fe55e46cecd1daf
parkjuram <parkjuram@gmail.com>
2014-04-30 18:37:36 +0900

    change payment relate file

--- classweek/custom_modules/INImx.py
+++ classweek/custom_modules/INImx.py
@@ -93,7 +93,7 @@ class INImx():
         self.logger.debug('INIPAYHOME : ' + self.inipayhome)
         self.logger.debug('P_MID : ' + self.id_merchant)
         self.logger.debug('P_STATUS : ' + self.status)
-        self.logger.debug('P_RMESG1 : ' + self.rmesg1)
+        self.logger.debug('P_RMESG1 : ' + self.rmesg1.decode('euc-kr').encode('utf-8'))
         self.logger.debug('P_TID : ' + self.tid)
         self.logger.debug('P_REQ_URL : ' + self.req_url)
         self.logger.debug('P_NOTI : ' + self.noti)
@@ -107,25 +107,8 @@ class INImx():
             'P_MID': self.id_merchant,
             'P_TID': self.tid
         }
-        response = requests.post( self.req_url, data=params)
-        response.encoding='euc-kr'
-        # response_body = repr(response.text).decode('utf-8').encode('utf-8')
-        # self.logger.debug(response_body)
-        # self.logger.debug('한글잘나오나')
-        # self.logger.debug(response.text)
-        # response_body = repr(response.text)
-        # self.logger.debug(response.encoding)
-        # # response_body = unicode(response_body, 'iso-8859-1').encode('utf-8')
-        # response_body = response_body.decode('iso-8859-1')
-        # response_body = response_body.encode('euc-kr')
-        # response_body = response_body.strip()
-        # self.logger.debug(response_body)
-        # self.logger.debug(response_body)
-        # self.logger.debug(response_body.strip())
-        # self.logger.debug(unicode(response_body.strip(), 'euc-kr'))
-        # self.logger.debug(unicode(response_body.strip(), 'utf-8'))
-        # self.logger.debug(response_body.strip().encode('euc-kr'))
-        # self.logger.debug(response_body.strip().encode('utf-8'))
+        response = requests.post(self.req_url, data=params)
+        response.encoding = 'euc-kr'
 
         params_list = response.text.strip().split('&')
         params_dict = {}
@@ -137,67 +120,51 @@ class INImx():
             params_dict[params_item_key]= params_item_value
 
         result_p_status = params_dict.get('P_STATUS', None)
-        self.logger.debug(result_p_status)
-        # result_p_tid = None
-        # result_p_type = None
-        # result_p_auth_dt = None
-        # result_p_mid = None
-        # result_p_oid = None
-        # result_p_amt = None
-        # result_p_uname = None
-        # result_p_rmesg1 = None
-        # result_p_rmesg2 = None
-        # result_p_noti = None
-        # result_p_fn_cd1 = None
-        # result_p_auth_no = None
-        # result_p_card_issuer_code = None
-        # result_p_card_num = None
-        # result_p_card_member_num = None
-        # result_p_card_purchase_code = None
-        # result_p_card_prtc_code = None
-        # result_p_hpp_corp_ = None
-        # result_p_vact_num = None
-        # result_p_vact_date = None
-        # result_p_vact_time = None
-        # result_p_vact_name = None
-        # result_p_vact_bank_code = None
+        result_p_tid = params_dict.get('P_TID', None)
+        result_p_type = params_dict.get('P_TYPE', None)
+        result_p_auth_dt = params_dict.get('P_AUTH_DT', None)
+        result_p_mid = params_dict.get('P_MID', None)
+        result_p_oid = params_dict.get('P_OID', None)
+        result_p_amt = params_dict.get('P_AMT', None)
+        result_p_uname = params_dict.get('P_UNAME', None)
+        result_p_rmesg1 = params_dict.get('P_RMESG1', None)
+        result_p_rmesg2 = params_dict.get('P_RMESG2', None)
+        result_p_noti = params_dict.get('P_NOTI', None)
+        result_p_fn_cd1 = params_dict.get('P_FN_CD1', None)
+        result_p_auth_no = params_dict.get('P_AUTH_NO', None)
+        result_p_card_issuer_code = params_dict.get('P_CARD_ISSUER_CODE', None)
+        result_p_card_num = params_dict.get('P_CARD_NUM', None)
+        result_p_card_member_num = params_dict.get('P_CARD_MEMBER_NUM', None)
+        result_p_card_purchase_code = params_dict.get('P_CARD_PURCHASE_CODE', None)
+        result_p_card_prtc_code = params_dict.get('P_CARD_PRTC_CODE', None)
+        result_p_hpp_corp_ = params_dict.get('P_HPP_CORP', None)
+        result_p_vact_num = params_dict.get('P_VACT_NUM', None)
+        result_p_vact_date = params_dict.get('P_VACT_DATE', None)
+        result_p_vact_time = params_dict.get('P_VACT_TIME', None)
+        result_p_vact_name = params_dict.get('P_VACT_NAME', None)
+        result_p_vact_bank_code = params_dict.get('P_VACT_CODE', None)
 
-        self.logger.debug(params_dict)
-        #
-        #
-        #
-        # params_dict = urlparse.parse_qsl(response.text.strip())[0]
-        # self.logger.debug(params_dict)
-        # self.logger.debug(params_dict.get('P_STATUS', 'status is none'))
-        # params_dict = {key.encode('utf-8'): value.encode('utf-8') for key, value in params_dict}
-        # self.logger.debug(params_dict)
-        # print params_dict.get('P_STATUS', 'status is none')
-
-        # params_dict = urlparse.parse_qsl(response.text)
-        # self.logger.debug(params_dict)
-        #
-        # params_dict = urlparse.parse_qsl(response.text)
-        # params_dict = {key.encode('utf-8'): value.encode('utf-8') for key, value in params_dict}
-        # self.logger.debug(params_dict)
-        #
-        #
-        # response.encode = 'utf-8'
-        #
-        # params_dict = urlparse.parse_qsl(response.text)
-        # self.logger.debug(params_dict)
-        #
-        # params_dict = {key.encode('utf-8'): value.encode('utf-8') for key, value in params_dict}
-        # self.logger.debug(params_dict)
-        #
-        # params_dict = urlparse.parse_qsl(response.text)
-        # params_dict = {key.encode('euc-kr'): value.encode('euc-kr') for key, value in params_dict}
-        # self.logger.debug(params_dict)
-
-        # response_data = urllib.urlopen(self.req_url, params).read()
-        # self.logger.debug(response_data)
-        # self.logger.debug(dir(response_data))
-        # conn = httplib.HTTPConnection("hostname:post")
-        # conn.request("POST", "/index.html", params, headers)
-        # response = conn.getresponse()
-        # self.logger.debug(response)
-        # self.logger.debug(dir(response))
+        self.logger.debug('P_STATUS : ' + result_p_status)
+        self.logger.debug('P_TID : ' + result_p_tid)
+        self.logger.debug('P_TYPE : ' + result_p_type)
+        self.logger.debug('P_AUTH_DT : ' + result_p_auth_dt)
+        self.logger.debug('P_MID : ' + result_p_mid)
+        self.logger.debug('P_OID : ' + result_p_oid)
+        self.logger.debug('P_AMT : ' + result_p_amt)
+        self.logger.debug('P_UNAME : ' + result_p_uname)
+        self.logger.debug('P_RMESG1 : ' + result_p_rmesg1)
+        self.logger.debug('P_RMESG2 : ' + result_p_rmesg2)
+        self.logger.debug('P_NOTI : ' + result_p_noti)
+        self.logger.debug('P_FN_CD1 : ' + result_p_fn_cd1)
+        self.logger.debug('P_AUTH_NO : ' + result_p_auth_no)
+        self.logger.debug('P_CARD_ISSUER_CODE : ' + result_p_card_issuer_code)
+        self.logger.debug('P_CARD_NUM : ' + result_p_card_num)
+        self.logger.debug('P_CARD_MEMBER_NUM : ' + result_p_card_member_num)
+        self.logger.debug('P_CARD_PURCHASE_CODE : ' + result_p_card_purchase_code)
+        self.logger.debug('P_CARD_PRTC_CODE : ' + result_p_card_prtc_code)
+        self.logger.debug('P_HPP_CORP : ' + result_p_hpp_corp_)
+        self.logger.debug('P_VACT_NUM : ' + result_p_vact_num)
+        self.logger.debug('P_VACT_DATE : ' + result_p_vact_date)
+        self.logger.debug('P_VACT_TIME : ' + result_p_vact_time)
+        self.logger.debug('P_VACT_NAME : ' + result_p_vact_name)
+        self.logger.debug('P_VACT_CODE : ' + result_p_vact_bank_code)
