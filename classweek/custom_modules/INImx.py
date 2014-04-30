22b37f0af310516fa6923742d35e85b0e0c8806d
parkjuram <parkjuram@gmail.com>
2014-04-30 12:41:57 +0900

    change payment relate file

--- classweek/custom_modules/INImx.py
+++ classweek/custom_modules/INImx.py
@@ -57,53 +57,35 @@ class INImx():
         self.P_STATUS = request.POST.get('P_STATUS', None)
         self.P_RMESG1 = request.POST.get('P_RMESG1', None)
         self.P_REQ_URL = request.POST.get('P_REQ_URL', None)
-        self.P_NOTI = request.POST.get('P_NOTE', None)
+        self.P_NOTI = request.POST.get('P_NOTI', None)
 
     def start_action(self):
 
         self.logger.debug('INImx def start_action(self):')
 
-        self.logger.debug('Start INImx_AUTH' + self.reqtype)
-        self.logger.debug('INIPAYHOME' + self.inipayhome)
-        self.logger.debug('P_MID' + self.id_merchant)
-        self.logger.debug('P_STATUS' + self.status)
-        self.logger.debug('P_RMESG1' + self.rmesg1)
-        self.logger.debug('P_TID' + self.tid)
-        self.logger.debug('P_REQ_URL' + self.req_url)
-        self.logger.debug('P_NOTI' + self.noti)
+        self.logger.debug('Start INImx_AUTH : ' + self.reqtype)
+        self.logger.debug('INIPAYHOME : ' + self.inipayhome)
+        self.logger.debug('P_MID : ' + self.id_merchant)
+        self.logger.debug('P_STATUS : ' + self.status)
+        self.logger.debug('P_RMESG1 : ' + self.rmesg1)
+        self.logger.debug('P_TID : ' + self.tid)
+        self.logger.debug('P_REQ_URL : ' + self.req_url)
+        self.logger.debug('P_NOTI : ' + self.noti)
         self.logger.debug('AUTH Transaction End')
 
         if self.reqtype == "PAY":
             self.do_pay()
 
     def do_pay(self):
-        params = urllib.urlencode({'P_MID': self.id_merchant, 'P_TID': self.tid})
-        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-        conn = httplib.HTTPConnection("hostname:post")
-        conn.request("POST","/index.html",params, headers)
-        response = conn.getresponse()
-        self.logger.debug( response )
-        self.logger.debug( dir(response) )
-        pass
-
-#     function startAction()
-# 	{
-# 		$this->printLog("Start INImx_AUTH ".$this->reqtype.VERSION);
-# 		$this->printLog("INIPAYHOME:".$this->inipayhome);
-# 		$this->printLog("P_MID:".$this->id_merchant);
-# 		$this->printLog("P_STATUS:".$this->status);
-# 		$this->printLog("P_RMESG1:".$this->rmesg1);
-# 		$this->printLog("P_TID:".$this->tid);
-# 		$this->printLog("P_REQ_URL:".$this->req_url);
-# 		$this->printLog("P_NOTI:".$this->noti);
-# 		$this->printLog("AUTH Transaction End");
-#
-# 		switch($this->reqtype)
-# 		{
-# 			case("PAY"):
-# 				$this->doPay();
-# 			break;
-#
-# 		}
-# //		$this->printLog("End INImx ".$this->reqtype.VERSION);
-# 	}
+        params = urllib.urlencode({
+            'P_MID': self.id_merchant,
+            'P_TID': self.tid
+        })
+        response_data = urllib.urlopen( self.req_url, params).read()
+        self.logger.debug(response_data)
+        self.logger.debug(dir(response_data))
+        # conn = httplib.HTTPConnection("hostname:post")
+        # conn.request("POST", "/index.html", params, headers)
+        # response = conn.getresponse()
+        # self.logger.debug(response)
+        # self.logger.debug(dir(response))
