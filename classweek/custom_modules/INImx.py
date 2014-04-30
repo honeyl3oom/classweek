d05f716d7cb4eaaee2a1b23a71d13fb435b7a0e2
parkjuram <parkjuram@gmail.com>
2014-04-30 18:03:25 +0900

    change payment relate file

--- classweek/custom_modules/INImx.py
+++ classweek/custom_modules/INImx.py
@@ -109,7 +109,7 @@ class INImx():
         # self.logger.debug(response_body.strip().encode('euc-kr'))
         # self.logger.debug(response_body.strip().encode('utf-8'))
 
-        params_dict = urlparse.parse_qsl(response.text)
+        params_dict = urlparse.parse_qsl(response.text.strip())
         self.logger.debug(params_dict)
         params_dict = {key.decode('euc-kr').encode('utf-8'): value.decode('euc-kr').encode('utf-8') for key, value in params_dict}
         self.logger.debug(params_dict)
