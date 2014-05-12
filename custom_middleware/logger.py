0ca995163b33014095163a5405fcbcc93036e8bd
parkjuram <parkjuram@gmail.com>
2014-05-12 16:54:59 +0900

    user session bug fix

--- custom_middleware/logger.py
+++ custom_middleware/logger.py
@@ -25,15 +25,20 @@ class ApiLogger(object):
         if hasattr(request, 'user') and request.user.is_authenticated():
             try:
                 user_session = UserSession.objects.get(user=request.user)
+
+                user_session_id = request.session.get('user_session_id', None)
+                if user_session_id is not None and user_session_id != user_session.user_session_id:
+                    user_session_id_have_to_combine = user_session_id
+
+                user_sessoin_id = user_session.user_session_id
+
             except ObjectDoesNotExist as e:
                 user_session = None
             #    raise error!!!!!
+                user_session_id = request.session.get('user_session_id', None)
+                if user_session_id is None:
+                    user_session_id = generate_session_id()
 
-            user_session_id = request.session.get('user_session_id', None)
-            if user_session_id is not None and user_session_id != user_session.user_session_id:
-                user_session_id_have_to_combine = user_session_id
-
-            user_sessoin_id = user_session.user_session_id
         else:
             user_session_id = request.session.get('user_session_id', None)
             if user_session_id is None:
