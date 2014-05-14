a2f5115c867a7cf9f5b3f25af353acd0b666c7ad
parkjuram <parkjuram@gmail.com>
2014-05-14 14:38:55 +0900

    attach send mail in inquire

--- classweek/common_method.py
+++ classweek/common_method.py
@@ -1,4 +1,4 @@
 from django.core.mail import send_mail
 
-def send_email(title, content, to=['blackpigstudio2014@gmail.com', 'parkjuram@gmail.com']):
+def send_email(title, content, to=['blackpigstudio2014@gmail.com']):
     send_mail(title, content, 'blackpigstudio2014@gmail.com', to )
