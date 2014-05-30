4b7426a149ea0260f8e19d363dc95ced7eb92f35
parkjuram <parkjuram@gmail.com>
2014-05-30 19:34:08 +0900

    edit setting in static directory

--- classweek/settings.py
+++ classweek/settings.py
@@ -189,10 +189,6 @@ TIME_ZONE = 'Asia/Seoul'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
 AUTH_PROFILE_MODULE = 'user.UserProfile'
 
 # @login_request
